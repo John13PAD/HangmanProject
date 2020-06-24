56bea210e7627ec08df05b7b3cb57dc5c3cd32a1
John13PAD <63984003+John13PAD@users.noreply.github.com>
2020-06-25 11:05:41 +1200

    Add files via upload

--- Name_V1.py
+++ Name_V1.py
@@ -2,19 +2,23 @@ from tkinter import *
 
 class username:
     def __init__(self, parent):
-        background = "#EEB8B8"
+        background = "#EECFBB"
         
         self.username_frame = Frame(bg = background, pady = 10)
         self.username_frame.grid()        
         
         #Main header
-        self.save_error_label = Label(self.username_frame, text = "Please enter your username", fg = "maroon", bg = background, font=("Lottes Handwriting", 19, "bold"),
+        self.username_label= Label(self.username_frame, text = "Enter your username", fg = "#FAF0E4", bg = background, font=("Lottes Handwriting", 19, "bold"),
                                                                pady=10, padx=10)
-        self.save_error_label.grid(row = 1, column=0)
+        self.username_label.grid(row = 1, column=0)
         
-        #Entry box for usernamehtee
-        self.to_convert_entry = Entry(self.username_frame, width = 20, font = ("Century Gothic", "14", ))
-        self.to_convert_entry.grid(row = 3)        
+        #Entry box for username
+        self.username_entry= Entry(self.username_frame, width = 20, font = ("Century Gothic", "14", ))
+        self.username_entry.grid(row = 3)        
+        
+        #Gets username from entry box
+        
+        
 if __name__ == "__main__":
     root = Tk()
     root.title("Hangman")
